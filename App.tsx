ad26c2b7b7d7af11a9ab12ee1c849810cf809c54
Jeff Phan <72235802+phanvanphuhn@users.noreply.github.com>
2025-09-06 14:36:18 +0700

    done ios

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   Platform,
   ImageSourcePropType,
+  Linking,
 } from "react-native";
 import {
   SafeAreaProvider,
@@ -23,6 +24,7 @@ import AiLayerIcon from "./assets/aiLayerIcon.png";
 
 import QuantumGif from "./assets/quanTum.gif";
 import WhyBmicGif from "./assets/whyBmic.gif";
+import AppIcon from "./assets/appIcon.png";
 
 type TProblems = {
   id: number;
@@ -89,6 +91,201 @@ function AppContent() {
     },
   ];
 
+  const whyChooseBMIC = [
+    {
+      id: 1,
+      title: "Compute Payments",
+      description: "Pay for quantum cloud services with BMIC tokens",
+      iconColor: "#3FE0C5",
+    },
+    {
+      id: 2,
+      title: "Staking Rewards",
+      description: "Stake BMIC tokens to earn rewards and secure the network",
+      iconColor: "#8B5CF6",
+    },
+    {
+      id: 3,
+      title: "Governance",
+      description: "Participate in protocol governance and decision making",
+      iconColor: "#9810fa",
+    },
+    {
+      id: 4,
+      title: "Node Access",
+      description: "Gain for premium computing power access and node types",
+      iconColor: "#FF6B6B",
+    },
+    {
+      id: 5,
+      title: "NFT Scheduling",
+      description: "Get NFTs for priority job scheduling on quantum nodes",
+      iconColor: "#3FE0C5",
+    },
+  ];
+
+  const tokenomicData = [
+    {
+      id: 1,
+      title: "Public Launch",
+      total: "300",
+      percent: "30%",
+    },
+    {
+      id: 2,
+      title: "Ecosystem Grants",
+      total: "250",
+      percent: "25%",
+    },
+    {
+      id: 3,
+      title: "Team & Advisors",
+      total: "200",
+      percent: "20%",
+    },
+    {
+      id: 4,
+      title: "Strategic Partnerships",
+      total: "150",
+      percent: "15%",
+    },
+    {
+      id: 5,
+      title: "Reserve / Liquidity",
+      total: "100",
+      percent: "10%",
+    },
+    {
+      id: 6,
+      title: "Total",
+      total: "1,500",
+      percent: "100%",
+    },
+  ];
+
+  const investmentData = [
+    {
+      id: 1,
+      title: "€40M",
+      subTitle: "Funding Raised",
+      description:
+        "Capital raised from institutional investors and strategic partners to fund quantum infrastructure development",
+    },
+    {
+      id: 2,
+      title: "10x",
+      subTitle: "Projected ROI",
+      description:
+        "Expected return on investment based on quantum computing market growth projections",
+    },
+    {
+      id: 3,
+      title: "First",
+      subTitle: "Competitive Edge",
+      description:
+        "World's first decentralized quantum cloud platform combining AI orchestration with blockchain governance",
+    },
+  ];
+
+  const roadmapData = [
+    {
+      id: 1,
+      title: "Phase 1: Foundation",
+      description: "Q4 2024 - Q1 2025",
+      items: [
+        {
+          id: 1,
+          title: "Build core AI & QML infrastructure",
+        },
+        {
+          id: 2,
+          title: "Alpha testnet launch - compute jobs",
+        },
+        {
+          id: 3,
+          title: "Whitepaper v1 and token launch",
+        },
+        {
+          id: 4,
+          title: "Arrangement of seed funding rounds",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Phase 2: Testnet Expansion",
+      description: "Q2 2024 - Q3 2024",
+      items: [
+        {
+          id: 1,
+          title: "Developer onboarding",
+        },
+        {
+          id: 2,
+          title: "Beta compute marketplace live",
+        },
+        {
+          id: 3,
+          title: "Expand partner networking",
+        },
+        {
+          id: 4,
+          title: "NFT-based access live",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Phase 3: Mainet Launch",
+      description: "Q4 2024 - Q1 2025",
+      items: [
+        {
+          id: 1,
+          title: "Full launch",
+        },
+        {
+          id: 2,
+          title: "Decentralized governance integration",
+        },
+        {
+          id: 3,
+          title: "Mainnet compute nodes are staking",
+        },
+        {
+          id: 4,
+          title: "Key strategic partnerships",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Phase 4: Optimization",
+      description: "Q4 2024 - Q1 2025",
+      items: [
+        {
+          id: 1,
+          title: "Global node expansion",
+        },
+        {
+          id: 2,
+          title: "Compute quantum and data networks",
+        },
+        {
+          id: 3,
+          title: "Hybrid AI quantum services",
+        },
+        {
+          id: 4,
+          title: "Key strategic partnerships",
+        },
+      ],
+    },
+  ];
+
+  const onSeeMore = () => {
+    void Linking.openURL("https://bmic.ai/");
+  };
+
   return (
     <SafeAreaView style={styles.f1}>
       <StatusBar style="light" />
@@ -97,6 +294,7 @@ function AppContent() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 32 }}
       >
+        {/* Header */}
         <Text style={[styles.title, { fontSize: 32 }]}>
           Powering the Decentralized Quantum Cloud
         </Text>
@@ -111,6 +309,7 @@ function AppContent() {
           BMICis building theworld’s firstdecentralized quantum compute network,
           secured by blockchain and orchestrated by AI.
         </Text>
+        {/* Why BMIC? */}
         <View style={[{ marginTop: 16, gap: 8 }]}>
           <Image
             source={WhyBmicGif}
@@ -135,6 +334,7 @@ function AppContent() {
             </Text>
           </View>
         </View>
+        {/* The Problem */}
         <View>
           <Text
             style={[styles.title, { marginVertical: 24, textAlign: "center" }]}
@@ -165,6 +365,7 @@ function AppContent() {
             ))}
           </View>
         </View>
+        {/* Our Solution */}
         <View>
           <Text
             style={[styles.title, { marginVertical: 24, textAlign: "center" }]}
@@ -197,12 +398,238 @@ function AppContent() {
             ))}
           </View>
         </View>
+        {/* Why choose BMIC token? */}
+        <View>
+          <Text
+            style={[styles.title, { marginVertical: 24, textAlign: "center" }]}
+          >
+            Why choose BMIC token?
+          </Text>
+          <View style={[{ gap: 16 }]}>
+            {whyChooseBMIC.map((whyChooseBMIC) => (
+              <View key={whyChooseBMIC.id} style={styles.card}>
+                <View
+                  style={{
+                    backgroundColor: whyChooseBMIC.iconColor,
+                    width: 16,
+                    height: 16,
+                    borderRadius: 16,
+                    marginVertical: 8,
+                    marginLeft: 16,
+                  }}
+                />
+                <View style={styles.cardTitleContainer}>
+                  <Text style={styles.title}>{whyChooseBMIC.title}</Text>
+                </View>
+                <View style={styles.cardTitleContainer}>
+                  <Text style={styles.description}>
+                    {whyChooseBMIC.description}
+                  </Text>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+        {/* Tokenomics */}
+        <View>
+          <Text
+            style={[
+              styles.title,
+              { marginVertical: 24, textAlign: "center", color: "#FFD700" },
+            ]}
+          >
+            Tokenomics
+          </Text>
+          <Text
+            style={[
+              styles.subtitle,
+              { textAlign: "center", marginBottom: 16, marginTop: -8 },
+            ]}
+          >
+            The BMIC token has a limited supply of 1.5 billion tokens and cannot
+            be increased.
+          </Text>
+          <View style={styles.wrapTokenomicsContainer}>
+            <View style={[styles.row, { alignItems: "center", gap: 8 }]}>
+              <Image
+                source={AppIcon}
+                style={{
+                  width: 32,
+                  height: 32,
+                }}
+              />
+              <Text style={styles.subtitle}>Coins</Text>
+            </View>
+            <View
+              style={[
+                styles.row,
+                styles.spaceBetween,
+                {
+                  alignItems: "center",
+                  paddingVertical: 16,
+                  borderBottomWidth: 1,
+                  borderBottomColor: "#71717b",
+                },
+              ]}
+            >
+              <View style={{ width: "50%", justifyContent: "flex-start" }}>
+                <Text style={styles.subtitle}>Category</Text>
+              </View>
+              <View style={{ width: "30%", alignItems: "flex-end" }}>
+                <Text style={styles.subtitle}>Million</Text>
+              </View>
+              <View style={{ width: "20%", alignItems: "flex-end" }}>
+                <Text style={styles.subtitle}>%</Text>
+              </View>
+            </View>
+            {tokenomicData.map((tokenomicItem) => (
+              <View
+                key={tokenomicItem.id}
+                style={[
+                  styles.row,
+                  styles.spaceBetween,
+                  {
+                    alignItems: "center",
+                    paddingVertical: 16,
+                    borderBottomWidth: 1,
+                    borderBottomColor: "#71717b",
+                  },
+                  tokenomicItem.id === tokenomicData.length && {
+                    borderBottomWidth: 0,
+                    backgroundColor: "#71717b",
+                  },
+                ]}
+              >
+                <View style={{ width: "50%", justifyContent: "flex-start" }}>
+                  <Text
+                    style={[
+                      styles.subtitle,
+                      tokenomicItem.id === tokenomicData.length && {
+                        fontWeight: "bold",
+                      },
+                    ]}
+                  >
+                    {tokenomicItem.title}
+                  </Text>
+                </View>
+                <View style={{ width: "30%", alignItems: "flex-end" }}>
+                  <Text
+                    style={[
+                      styles.subtitle,
+                      tokenomicItem.id === tokenomicData.length && {
+                        fontWeight: "bold",
+                      },
+                    ]}
+                  >
+                    {tokenomicItem.total}
+                  </Text>
+                </View>
+                <View style={{ width: "20%", alignItems: "flex-end" }}>
+                  <Text
+                    style={[
+                      styles.subtitle,
+                      tokenomicItem.id === tokenomicData.length && {
+                        fontWeight: "bold",
+                      },
+                    ]}
+                  >
+                    {tokenomicItem.percent}
+                  </Text>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+        {/* Investment Opportunity */}
+        <View>
+          <Text
+            style={[styles.title, { marginVertical: 24, textAlign: "center" }]}
+          >
+            Investment Opportunity
+          </Text>
+          <View style={{ gap: 16 }}>
+            {investmentData.map((investmentItem) => (
+              <View style={styles.investmentContainer}>
+                <Text style={[styles.title, { color: "#3FE0C5" }]}>
+                  {investmentItem.title}
+                </Text>
+                <Text style={styles.subtitle}>{investmentItem.subTitle}</Text>
+                <Text style={[styles.description, { color: "#71717b" }]}>
+                  {investmentItem.description}
+                </Text>
+              </View>
+            ))}
+          </View>
+        </View>
+        {/* Roadmap */}
+        <View>
+          <Text
+            style={[styles.title, { marginVertical: 24, textAlign: "center" }]}
+          >
+            Roadmap
+          </Text>
+          <View style={{ gap: 16 }}>
+            {roadmapData.map((roadmapItem) => (
+              <View style={styles.warpRoadmapItem}>
+                <View>
+                  <View style={[styles.row, { alignItems: "center", gap: 8 }]}>
+                    <View
+                      style={{
+                        width: 32,
+                        height: 32,
+                        backgroundColor:
+                          roadmapItem.id === 1
+                            ? "#3FE0C5"
+                            : roadmapItem.id === 2
+                            ? "#8B5CF6"
+                            : roadmapItem.id === 3
+                            ? "#9810fa"
+                            : "#FF6B6B",
+                        borderRadius: 32,
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Text style={styles.subtitle}>{roadmapItem.id}</Text>
+                    </View>
+                    <View>
+                      <Text style={styles.subtitle}>{roadmapItem.title}</Text>
+                      <Text style={[styles.description, { color: "#71717b" }]}>
+                        {roadmapItem.description}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{ gap: 8, marginTop: 8 }}>
+                    {roadmapItem.items.map((item) => (
+                      <View
+                        style={[styles.row, { gap: 8, alignItems: "center" }]}
+                      >
+                        <View
+                          style={{
+                            width: 8,
+                            height: 8,
+                            backgroundColor: "#71717b",
+                            borderRadius: 16,
+                          }}
+                        />
+                        <Text style={[styles.description, { color: "white" }]}>
+                          {item.title}
+                        </Text>
+                      </View>
+                    ))}
+                  </View>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
       </ScrollView>
       <TouchableOpacity
         style={[
           styles.bottomButton,
           Platform.OS === "android" ? { marginBottom: insets.bottom } : {},
         ]}
+        onPress={onSeeMore}
       >
         <Text style={styles.subtitle}>See More Information</Text>
       </TouchableOpacity>
@@ -279,5 +706,22 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     paddingHorizontal: 16,
     paddingVertical: 8,
+  },
+  wrapTokenomicsContainer: {
+    backgroundColor: "#18181b",
+    borderRadius: 16,
+    padding: 16,
+  },
+  investmentContainer: {
+    borderRadius: 16,
+    padding: 16,
+    backgroundColor: "#1A2332",
+    gap: 8,
+  },
+  warpRoadmapItem: {
+    borderRadius: 16,
+    padding: 16,
+    backgroundColor: "#23273C",
+    gap: 8,
   },
 });
